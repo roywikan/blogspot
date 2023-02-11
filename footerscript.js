0b6cd2f2ead501bb45da947e873b5c1f604c6bbb
roywikan <roywikan@users.noreply.github.com>
2023-02-11 12:38:39 +0700

    Update footerscript.js

--- footerscript.js
+++ footerscript.js
@@ -1,5 +1,3 @@
-
-<script>
 //<![CDATA[
 function downloadJSAtOnload(){var d=document.createElement("script");d.src="https://cdn.staticaly.com/gh/roywikan/blogspot/main/relatedarticles.min.js",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
 //]]>
@@ -13,4 +11,3 @@ while (oldParent.childNodes.length) { newParent.appendChild(oldParent.firstChild
 };
 function expandComment(){var e=document.getElementById("overflowbox");if(e.classList)e.classList.toggle("expand-more");else{var s=e.className.split(" "),a=s.indexOf("expand-more");a>=0?s.splice(a,1):s.push("expand-more"),e.className=s.join(" ")}}
 //]]>
-</script>
